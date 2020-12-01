77157f6958070107bdf793a5d95417443a9abff5
Myeongwon Choi <auspic7@gmail.com>
2020-12-01 22:42:56 +0900

    Add XML Exporter

---
+++ DP2020Project/src/me/wonny/test/XMLExporter.java
@@ -0,0 +1,46 @@
+package me.wonny.test;
+
+import com.holub.database.Table;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class XMLExporter implements Table.Exporter {
+    private final Writer out;
+    private String tableName;
+    private List<String> columnNames = new ArrayList<String>();
+
+    public XMLExporter(Writer out) {
+        this.out = out;
+    }
+
+    @Override
+    public void storeMetadata(String tableName, int width, int height, Iterator columnNames) throws IOException {
+        this.tableName = tableName;
+        while (columnNames.hasNext()){
+            this.columnNames.add(columnNames.next().toString());
+        }
+        out.write("<" + tableName + ">\n");
+    }
+
+    @Override
+    public void storeRow(Iterator data) throws IOException {
+        out.write("<row>\n");
+        for(String columnName: columnNames){
+            String text = data.next().toString();
+            out.write("<"+columnName+">"+text+"</"+columnName+">\n");
+        }
+        out.write("</row>\n");
+    }
+
+    @Override
+    public void startTable() throws IOException { }
+
+    @Override
+    public void endTable() throws IOException {
+        out.write("</" + tableName + ">\n");
+    }
+}
---
+++ DP2020Project/src/me/wonny/test/XMLExporterTest.java
@@ -0,0 +1,27 @@
+package me.wonny.test;
+
+import com.holub.database.Table;
+import com.holub.database.TableFactory;
+import me.wonny.HTMLExporter;
+import org.junit.Test;
+
+import java.io.FileWriter;
+import java.io.Writer;
+
+public class XMLExporterTest {
+    @Test
+    public void exportPeopleTable() {
+        try {
+            Table people = TableFactory.create("people", new String[] { "last", "first", "addrId" });
+            people.insert(new Object[] { "Holub", "Allen", "1" });
+            people.insert(new Object[] { "Flintstone", "Wilma", "2" });
+            people.insert(new String[] { "addrId", "first", "last" }, new Object[] { "2", "Fred", "Flintstone" });
+            Writer writer = new FileWriter("c:/dp2020/people.xml");
+            people.export(new XMLExporter(writer));
+            writer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
