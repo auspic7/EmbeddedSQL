b0309ffdcad0d9e173250884f53bae54028b306a
Myeongwon Choi <auspic7@gmail.com>
2020-12-15 16:55:48 +0900

    Resolve code repetition

--- DP2020Project/src/me/wonny/test/DatabaseTest.java
+++ DP2020Project/src/me/wonny/test/DatabaseTest.java
@@ -2,7 +2,9 @@ package me.wonny.test;
 
 import com.holub.database.Database;
 import com.holub.database.jdbc.JDBCStatement;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -10,12 +12,15 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DatabaseTest {
-    @Test
-    public void insertAndSelectAddress() {
+    Database database = null;
+    Statement statement = null;
+
+    @Before
+    public void initDBAndTable() {
         try {
             Class.forName("com.holub.database.jdbc.JDBCDriver").newInstance();
-            Database database = new Database("c:/dp2020");
-            Statement statement = new JDBCStatement(database);
+            database = new Database("c:/dp2020");
+            statement = new JDBCStatement(database);
             statement.executeUpdate("create table address (addrId int, street varchar, city varchar, state char(2), zip int, primary key(addrId))");
             statement.executeUpdate("create table name(first varchar(10), last varchar(10), addrId integer)");
             statement.executeUpdate("insert into address values( 0,'12 MyStreet','Berkeley','CA','99999')");
@@ -23,7 +28,14 @@ public class DatabaseTest {
             statement.executeUpdate("insert into name VALUES ('Fred',  'Flintstone', '1')");
             statement.executeUpdate("insert into name VALUES ('Wilma', 'Flintstone', '1')");
             statement.executeUpdate("insert into name (last,first,addrId) VALUES('Holub','Allen',(10-10*1))");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void insertAndSelectAddress() {
+        try {
             ResultSet resultSet = statement.executeQuery("SELECT state FROM address");
             resultSet.next();
             assertEquals(resultSet.getString("state"), "CA");
@@ -35,17 +47,6 @@ public class DatabaseTest {
     @Test
     public void insertAndJoinDuplicateName() {
         try {
-            Class.forName("com.holub.database.jdbc.JDBCDriver").newInstance();
-            Database database = new Database("c:/dp2020");
-            Statement statement = new JDBCStatement(database);
-            statement.executeUpdate("create table address (addrId int, street varchar, city varchar, state char(2), zip int, primary key(addrId))");
-            statement.executeUpdate("create table name(first varchar(10), last varchar(10), addrId integer)");
-            statement.executeUpdate("insert into address values( 0,'12 MyStreet','Berkeley','CA','99999')");
-            statement.executeUpdate("insert into address values( 1, '34 Quarry Ln.', 'Bedrock' , 'XX', '00000')");
-            statement.executeUpdate("insert into name VALUES ('Fred',  'Flintstone', '1')");
-            statement.executeUpdate("insert into name VALUES ('Wilma', 'Flintstone', '1')");
-            statement.executeUpdate("insert into name (last,first,addrId) VALUES('Holub','Allen',(10-10*1))");
-
             ResultSet resultSet = statement.executeQuery("select * from address,name where address.addrId = name.addrId\n");
             System.out.println(resultSet.next());
             assertEquals(resultSet.getString("addrId"), "0");
@@ -54,8 +55,6 @@ public class DatabaseTest {
             assertEquals(resultSet.getString("zip"), "99999");
             assertEquals(resultSet.getString("first"), "Allen");
             assertEquals(resultSet.getString("last"), "Holub");
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
